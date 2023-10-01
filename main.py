6e71bc42eaa8e8e653b105bd552c5d52332c33ed
Uzair <uzair.m6d@gmail.com>
2023-10-01 13:28:32 -0400

    add chatbot pages

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Welcome to Workshop 3: Deploying Language Apps")
+
+st.write("To get started, take a look at this workshop's [walkthrough](https://msu-ai.notion.site/LangChain-Apps-MSU-AI-Club-7fbdc17aa0f6415a8d7606cfb1e41e00)")
+
+
+
